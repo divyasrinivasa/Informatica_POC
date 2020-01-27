45a5ac4967f71c0ccf60d41f2afb29b5df623994
divyasrinivasa <59303982+divyasrinivasa@users.noreply.github.com>
2020-01-27 09:21:40 +0530

    Add files via upload

---
+++ merge_import_from_git_to_iics.bat
@@ -0,0 +1,13 @@
+#merge changes from dev branch to test branch
+cd C:\Informatica_POC\Informatica_POC
+git checkout test
+git merge dev
+git diff-index --quiet HEAD || git commit -m "Merge file from Dev Branch to Test"
+git push origin test
+
+#import changes from git test branch to iics test env
+cd C:\Informatica_POC\Test\Informatica_POC
+git checkout test
+git pull origin test
+cd C:\Informatica_POC\IICS Utility
+iics import --podHostName apse1.dm-ap.informaticacloud.com -r us -u srinidhi.narendra@in.ibm.com -p Welkom123 -z "C:\Informatica_POC\Test\Informatica_POC\IICS_Dev_export.zip"
